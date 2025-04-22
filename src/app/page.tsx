e4533ab83c3d11bcd28cae19d1dcd31cf7b836d3
Gonzagadavid <gonzagadaviddev@gmail.com>
2025-04-21 23:36:15 -0300

    feat: remove admin redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,8 @@ import AboutSection from "@/components/custom/aboutSection";
 import ProcessSection from "@/components/custom/processSection";
 import HelpSection from "@/components/custom/helpSection";
 import Footer from "@/components/custom/footer";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { Routes } from "@/constants/routes";
-import { checkPermissions, UserRole } from "@/constants/permissions";
 
 export default function Home() {
-  const session = useSession();
-
-  if (
-    session?.data?.user &&
-    checkPermissions(UserRole.VOLUNTEER, session.data.user.role)
-  ) {
-    redirect(Routes.ADMIN);
-  }
   return (
     <div>
       <Header />
