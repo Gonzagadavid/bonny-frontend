f23e4a86adbddbe9070452afb5137003dfb57f76
Gonzagadavid <gonzagadaviddev@gmail.com>
2025-05-04 14:16:14 -0300

    fix: adjust admin home page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,8 +5,10 @@ import React from "react";
 export default function DashboardPage() {
   return (
     <div className="container mx-auto py-12 px-4">
-      <h1>Dashboard Administrativo</h1>
-      <p>Bem-vindo ao painel de administração da ONG!</p>
+      <section className="mb-16 max-w-5xl mx-auto">
+        <h1>Dashboard Administrativo</h1>
+        <p>Bem-vindo ao painel de administração da ONG!</p>
+      </section>
     </div>
   );
 }
