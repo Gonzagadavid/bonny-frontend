54f05d8c08046a2b6c36f4a552baadd9118253c6
Gonzagadavid <gonzagadaviddev@gmail.com>
2025-05-05 00:22:36 -0300

    fix: remove unused animal info

--- src/app/admin/animals/animal-details/[id]/page.tsx
+++ src/app/admin/animals/animal-details/[id]/page.tsx
@@ -1,11 +1,7 @@
 import AnimalInfo from "../../_components/animalInfo";
 import Candidacies from "../../_components/candidacies";
-import DonationList from "../../_components/donationList";
-import SponsorshipList from "../../_components/sponsordhipList";
 import { getAnimal } from "../../_lib/getAnimal";
 import { getCandidacies } from "../../_lib/getCandidacies";
-import { listDonationByAnimal } from "../../_lib/listDonationByAnimal";
-import { listSponsorshipByAnimal } from "../../_lib/listSponsorshipByAnimal";
 
 export default async function AnimalDetails({
   params,
@@ -15,8 +11,6 @@ export default async function AnimalDetails({
   const { id } = await params;
   const animalInfo = await getAnimal(id);
   const candidacies = await getCandidacies(id);
-  const sponsorships = await listSponsorshipByAnimal(id);
-  const donations = await listDonationByAnimal(id);
 
   return (
     <div className="container mx-auto py-12 px-4">
@@ -26,8 +20,6 @@ export default async function AnimalDetails({
         </h2>
         <div className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow">
           <AnimalInfo animal={animalInfo} />
-          <SponsorshipList sponsorships={sponsorships} />
-          <DonationList donations={donations} />
           <Candidacies candidacies={candidacies} />
         </div>
       </section>
